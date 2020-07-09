323899f40f6b5c20ff601edbc4786f5eda7ba4e9
qiang1014 <13100602291@163.com>
2020-07-09 11:30:16 +0800

    add

---
+++ src/main/java/cn/zhaoq/mycommunity/controller/HelloController.java
@@ -0,0 +1,15 @@
+package cn.zhaoq.mycommunity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "index";
+    }
+}
